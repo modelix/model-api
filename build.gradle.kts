c4aac9f8f5dc5f1c78bb0198100abcd0f05ac2af
Sascha Lisson <sl@slisson.de>
2022-08-26 18:59:14 +0200

    meta model generator
    The goal is to generate a typed API for the model from
    some language definition.
    Something similar is already implemented for MPS in
    https://github.com/modelix/api-gen
    The new implementation is MPS independent.
    It's a lightweight generator implemented in Kotlin that takes
    JSON/YAML as an input and generates Kotlin classes.
    On top of these classes other meta DSLs (e.g. for the editor)
    can be implemented as internal Kotlin DSLs.
    
    Moved from the modelix main repository to here.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
 
 plugins {
-    id("maven-publish")
-    id("org.jetbrains.kotlin.multiplatform") version "1.6.21"
-    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    `maven-publish`
+    kotlin("multiplatform") version "1.6.21" apply false
+    kotlin("plugin.serialization") version "1.6.21" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0" apply false
     id("com.palantir.git-version") version "0.15.0"
-}
-
-configurations {
-    ktlint
 }
 
 group = "org.modelix"
@@ -20,77 +17,10 @@ fun computeVersion(): Any {
     val versionFile = file("version.txt")
     val gitVersion: groovy.lang.Closure<String> by extra
     var version = if (versionFile.exists()) versionFile.readText().trim() else gitVersion()
-    if (!"true".equals(project.findProperty("ciBuild"))) {
+    if (!versionFile.exists() && !"true".equals(project.findProperty("ciBuild"))) {
         version = "$version-SNAPSHOT"
     }
     return version
-}
-
-repositories {
-    mavenCentral()
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-}
-
-ktlint {
-    disabledRules.add("no-wildcard-imports")
-    outputToConsole.set(true)
-}
-
-tasks.named("check") {
-    dependsOn("ktlintCheck")
-}
-
-kotlin {
-    jvm()
-    js(BOTH) {
-        nodejs {
-            testTask {
-                useMocha {
-                    timeout = "10000"
-                }
-            }
-        }
-    }
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-                implementation("io.github.microutils:kotlin-logging:2.1.21")
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-        val jvmMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-jdk8"))
-            }
-        }
-        val jvmTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation(kotlin("test-junit"))
-            }
-        }
-        val jsMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-js"))
-            }
-        }
-        val jsTest by getting {
-            dependencies {
-                implementation(kotlin("test-js"))
-            }
-        }
-    }
 }
 
 fun getGithubCredentials(): Pair<String, String>? {
@@ -105,31 +35,43 @@ fun getGithubCredentials(): Pair<String, String>? {
     return null
 }
 
-publishing {
+allprojects {
     repositories {
-        if (project.hasProperty("artifacts.itemis.cloud.user")) {
-            maven {
-                name = "itemisNexus3"
-                url = if (version.toString().contains("SNAPSHOT"))
-                    uri("https://artifacts.itemis.cloud/repository/maven-mps-snapshots/")
-                else
-                    uri("https://artifacts.itemis.cloud/repository/maven-mps-releases/")
-                credentials {
-                    username = project.findProperty("artifacts.itemis.cloud.user").toString()
-                    password = project.findProperty("artifacts.itemis.cloud.pw").toString()
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "maven-publish")
+    version = rootProject.version
+    group = rootProject.group
+    publishing {
+        repositories {
+            if (project.hasProperty("artifacts.itemis.cloud.user")) {
+                maven {
+                    name = "itemisNexus3"
+                    url = if (version.toString().contains("SNAPSHOT"))
+                        uri("https://artifacts.itemis.cloud/repository/maven-mps-snapshots/")
+                    else
+                        uri("https://artifacts.itemis.cloud/repository/maven-mps-releases/")
+                    credentials {
+                        username = project.findProperty("artifacts.itemis.cloud.user").toString()
+                        password = project.findProperty("artifacts.itemis.cloud.pw").toString()
+                    }
                 }
             }
-        }
-        val githubCredentials = getGithubCredentials()
-        if (githubCredentials != null) {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/modelix/model-api")
-                credentials {
-                    username = githubCredentials.first
-                    password = githubCredentials.second
+            val githubCredentials = getGithubCredentials()
+            if (githubCredentials != null) {
+                maven {
+                    name = "GitHubPackages"
+                    url = uri("https://maven.pkg.github.com/modelix/model-api")
+                    credentials {
+                        username = githubCredentials.first
+                        password = githubCredentials.second
+                    }
                 }
             }
         }
     }
 }
+
---
+++ metamodel-generator/src/main/kotlin/org/modelix/metamodel/definition/Language.kt
@@ -0,0 +1,58 @@
+package org.modelix.metamodel.definition
+
+import com.charleskorn.kaml.Yaml
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.io.File
+
+@Serializable
+data class Language(
+    val name: String,
+    val concepts: List<Concept>,
+) {
+    companion object {
+        fun fromFile(file: File): Language {
+            return when (file.extension.lowercase()) {
+                "yaml" -> Yaml.default.decodeFromString(file.readText())
+                "json" -> Json.decodeFromString(file.readText())
+                else -> throw IllegalArgumentException("Unsupported file extension: $file")
+            }
+        }
+    }
+}
+
+@Serializable
+data class Concept(
+    val name: String,
+    val abstract: Boolean = false,
+    val properties: List<Property> = emptyList(),
+    val children: List<Child> = emptyList(),
+    val references: List<Reference> = emptyList(),
+    val extends: List<String> = emptyList(),
+)
+
+@Serializable
+data class Property(
+    val name: String,
+    val type: PropertyType = PropertyType.STRING
+)
+
+enum class PropertyType {
+    STRING,
+}
+
+@Serializable
+data class Child(
+    val name: String,
+    val type: String,
+    val multiple: Boolean = false,
+    val optional: Boolean = true,
+)
+
+@Serializable
+data class Reference(
+    val name: String,
+    val type: String,
+    val optional: Boolean = true,
+)
