4547d2228d5ad2e172adfc3b51666e652310c754
Sascha Lisson <sl@slisson.de>
2022-09-03 12:32:42 +0200

    Pull TypedNodeImpl._concept up into ITypedNode._concept

--- metamodel-runtime/src/commonMain/kotlin/org/modelix/metamodel/ITypedNode.kt
+++ metamodel-runtime/src/commonMain/kotlin/org/modelix/metamodel/ITypedNode.kt
@@ -3,5 +3,6 @@ package org.modelix.metamodel
 import org.modelix.model.api.INode
 
 interface ITypedNode {
+    val _concept: ITypedConcept
     val _node: INode
 }
--- metamodel-runtime/src/commonMain/kotlin/org/modelix/metamodel/TypedNodeImpl.kt
+++ metamodel-runtime/src/commonMain/kotlin/org/modelix/metamodel/TypedNodeImpl.kt
@@ -3,7 +3,6 @@ package org.modelix.metamodel
 import org.modelix.model.api.INode
 
 abstract class TypedNodeImpl(override val _node: INode) : ITypedNode {
-    abstract val _concept: ITypedConcept
 
     init {
         require(_node.concept == _concept) { "Concept of node $_node expected to be $_concept, but was ${_node.concept}" }
