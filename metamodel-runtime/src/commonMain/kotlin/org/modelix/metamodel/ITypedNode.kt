8fa3cfaef94ab08394fad8163a26f235c71fb7c5
Sascha Lisson <sl@slisson.de>
2022-09-03 12:33:46 +0200

    ITypedNode.getPropertyValue(IProperty)
    There is only getPropertyValue(String) in INode

--- metamodel-runtime/src/commonMain/kotlin/org/modelix/metamodel/ITypedNode.kt
+++ metamodel-runtime/src/commonMain/kotlin/org/modelix/metamodel/ITypedNode.kt
@@ -1,8 +1,11 @@
 package org.modelix.metamodel
 
 import org.modelix.model.api.INode
+import org.modelix.model.api.IProperty
 
 interface ITypedNode {
     val _concept: ITypedConcept
     val _node: INode
 }
+
+fun ITypedNode.getPropertyValue(property: IProperty): String? = _node.getPropertyValue(property.name)
